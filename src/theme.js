9c1b0a012e2b67694b2e1828e2955de3377fe4f0
Matthew Kerns <matthewkerns@ymail.com>
2025-06-18 17:32:41 -0400

    Fix typo in color comment

--- src/theme.js
+++ src/theme.js
@@ -2,7 +2,7 @@ import { createTheme } from "@mui/material/styles";
 
 const theme = createTheme({
   palette: {
-    primary: { main: "#00843D" }, // Kelley Green
+    primary: { main: "#00843D" }, // Kelly Green
     secondary: { main: "#FFFFFF" }, // White
     text: { primary: "#000000" }, // Black
   },
